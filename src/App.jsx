184facd84ce7ca888cba867051735945159dad07
Patrycja Zborowska <zborowskapati@gmail.com>
2026-05-24 13:51:24 +0200

    fix(grid layout): pokemons grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,50 @@
 import Navbar from "./components/Navbar/Navbar.jsx";
 import Searchbar from "./components/Searchbar/Searchbar.jsx";
+import { usePokemonData } from "./services/usePokemonData.js";
+import { PokemonCard } from "./components/PokemonCard/PokemonCard.jsx";
 import "./app.css";
 
 function App() {
+    // Pobieramy dane z naszego hooka
+    const { pokemons, isLoading, error, loadMore, hasMore } = usePokemonData();
 
-  return (
-    <>
-        <Navbar></Navbar>
-        <main className="main-layout">
+    return (
+        <>
+            <Navbar />
+            <main className="main-layout">
 
-            {/* Lewa kolumna: 70% */}
-            <section className="pokedex-column">
-                <Searchbar></Searchbar>
-                <div className="pokedex-grid"></div>
-            </section>
+                {/* Lewa kolumna: 70% */}
+                <section className="pokedex-column">
+                    <Searchbar />
 
-            {/* Prawa kolumna: 30% */}
-            <aside className="team-column">
-                {/* Tutaj będzie kontener "Twoja Drużyna (6/6)" */}
-            </aside>
+                    {error && <p>Błąd: {error}</p>}
 
-        </main>
-    </>
-  )
+                    {/* Wrzucamy wygenerowane karty do siatki */}
+                    <div className="pokedex-grid">
+                        {pokemons.map(pokemon => (
+                            <PokemonCard key={pokemon.id} pokemon={pokemon} />
+                        ))}
+                    </div>
+
+                    {/* Sekcja ładowania pod siatką */}
+                    <div className="pagination">
+                        {isLoading && <p>Loading Pokedex...</p>}
+                        {!isLoading && hasMore && (
+                            <button className="load-more-btn" onClick={loadMore}>
+                                Załaduj więcej
+                            </button>
+                        )}
+                    </div>
+                </section>
+
+                {/* Prawa kolumna: 30% */}
+                <aside className="team-column">
+                    {/* Tutaj będziemy dodawać wybrane Pokemony */}
+                </aside>
+
+            </main>
+        </>
+    )
 }
 
-export default App
+export default App;
